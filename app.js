d9764e8a3d59a3c18e573cb637e2c90eb07fc976
Clonmorfo <salguerojuanfrancisco@gmail.com>
2021-09-07 21:32:25 -0300

    se setea el entorno y se crea el /home

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+
+/*Config public*/
+app.use(express.static(path.resolve(__dirname, "public")))
+/* Routes */
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+
+
+
+app.listen(3000, () =>{
+    console.log("Server corriendo en el puerto 3000");
+})
